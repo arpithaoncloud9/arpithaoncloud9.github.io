13cdced8aa0283c673229db69f24c43f6e11924c
arpithaoncloud9 <arpithaoncloud9@gmail.com>
2026-06-26 12:42:13 -0400

    Add new certification URL

--- src/data/certifications.js
+++ src/data/certifications.js
@@ -1,5 +1,15 @@
 export const certifications = [
   {
+    src: "./images/aws-certified-devops-engineer-proessional.png",
+    alt: "AWS Certified DevOps Engineer Professional",
+    link: "https://www.credly.com/badges/6e40efdf-e7b2-4748-8cb0-172590d205a9",
+  },
+  {
+    src: "./images/aws-certified-solutions-architect-associate.png",
+    alt: "AWS Certified Solutions Architect Associate",
+    link: "https://www.credly.com/badges/22f63035-f5af-4d21-834c-f07f9b9270f1/public_url",
+  },
+  {
     src: "./images/aws-certified-cloudops-engineer-associate.png",
     alt: "AWS Certified CloudOps Engineer Associate",
     link: "https://www.credly.com/earner/earned/badge/5463f0d0-a9b4-4dd8-a682-ce1365cfd338",
@@ -8,11 +18,6 @@ export const certifications = [
     src: "./images/aws-certified-ai-practitioner.png",
     alt: "AWS Certified AI Practitioner",
     link: "https://www.credly.com/badges/6e46f52f-87f2-4987-83c7-35d9976c3406/public_url",
-  },
-  {
-    src: "./images/aws-certified-solutions-architect-associate.png",
-    alt: "AWS Certified Solutions Architect Associate",
-    link: "https://www.credly.com/badges/22f63035-f5af-4d21-834c-f07f9b9270f1/public_url",
   },
   {
     src: "./images/cloud-digital-leader-certification.png",
@@ -25,7 +30,7 @@ export const certifications = [
     link: "https://www.credly.com/badges/2d83d54d-0155-4d3a-8548-f3fdb00390d1/public_url",
   },
   {
-    src: "./images/microsoft-certified-fundamentals-badge.svg",
+    src: "./images/microsoft-certified-fundamentals-badge.png",
     alt: "Microsoft Certified Azure Fundamentals",
     link: "https://learn.microsoft.com/api/credentials/share/en-us/MariaArpithaSagayam-9407/CF7812BB393EBA89?sharingId=E866A35C9F6DED0F",
   },
