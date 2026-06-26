a7ad21f02ddd3cac6a5c6cd6f227125bffcfbb31
arpithaoncloud9 <arpithaoncloud9@gmail.com>
2026-06-26 12:58:23 -0400

    Add README

--- src/data/certifications.js
+++ src/data/certifications.js
@@ -1,6 +1,6 @@
 export const certifications = [
   {
-    src: "./images/aws-certified-devops-engineer-proessional.png",
+    src: "./images/aws-certified-devops-engineer-professional.png",
     alt: "AWS Certified DevOps Engineer Professional",
     link: "https://www.credly.com/badges/6e40efdf-e7b2-4748-8cb0-172590d205a9",
   },
@@ -30,7 +30,7 @@ export const certifications = [
     link: "https://www.credly.com/badges/2d83d54d-0155-4d3a-8548-f3fdb00390d1/public_url",
   },
   {
-    src: "./images/microsoft-certified-fundamentals-badge.png",
+    src: "./images/microsoft-certified-fundamentals-badge.svg",
     alt: "Microsoft Certified Azure Fundamentals",
     link: "https://learn.microsoft.com/api/credentials/share/en-us/MariaArpithaSagayam-9407/CF7812BB393EBA89?sharingId=E866A35C9F6DED0F",
   },
